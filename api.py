d9cc86d4fe3d7d1f932f7f902bb4cfb669f1613e
Andi <andi.basic10@gmail.com>
2019-02-27 13:27:11 +0100

    osnovni CRUD

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ app.config['MYASQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
-@app.route('/knjige', methods=["GET"])
+@app.route('/knjiga', methods=["GET"])
 def sve_knjige_prikaz():
     conn = mysql.connect()
     cursor = conn.cursor()
@@ -64,6 +64,155 @@ def brisanje_knjige(sifra):
     cursor.execute("DELETE FROM knjiga WHERE sifra=%s", sifra)
     conn.commit()
     return ("Knjiga je obrisana")
+
+@app.route('/izdavatelj', methods=["GET"])
+def svi_izdavatelji_prikaz():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM izdavatelj")
+    izdavatelj = cursor.fetchall()
+    conn.commit()
+    return jsonify({'izdavatelji' : izdavatelj})
+
+@app.route('/izdavatelj/<sifra>', methods=["GET"])
+def jedan_izdavatelj_prikaz(sifra):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    izdavatelj = cursor.execute("SELECT * FROM izdavatelj where sifra = %s", sifra)
+    izdavatelj = cursor.fetchone()
+    return jsonify({'izdavatelj' : izdavatelj})
+
+@app.route('/izdavatelj', methods=["POST"])
+def dodavanje_izdavitelj():
+    ime = request.json.get('Ime', None)
+    prezime = request.json.get('Prezime', None)
+    adresa = request.json.get('Adresa', None)
+    mjesto = request.json.get('Mjesto', None)
+    postanski_broj = request.json.get('Postanski_broj', None)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO izdavatelj (Ime, Prezime, Adresa, Mjesto, Postanski_broj) VALUES (%s,%s,%s,%s,%s)", (ime,prezime,adresa,mjesto,postanski_broj))
+    conn.commit()
+    return ("Novi izdavatelj dodan")
+
+@app.route('/izdavatelj/<sifra>', methods=["PUT"])
+def edit_izdavatelj(sifra):
+    ime = request.json.get('Ime', None)
+    prezime = request.json.get('Prezime', None)
+    adresa = request.json.get('Adresa', None)
+    mjesto = request.json.get('Mjesto', None)
+    postanski_broj = request.json.get('Postanski_broj', None)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE izdavatelj SET Ime=%s, Prezime=%s, Adresa=%s, Mjesto=%s, Postanski_broj=%s WHERE sifra =%s", (ime,prezime,adresa,mjesto,postanski_broj,sifra))
+    cursor.fetchone()
+    conn.commit()
+    return jsonify("Izdavatelj je uređen")
+
+@app.route('/izdavatelj/<sifra>', methods=["DELETE"])
+def brisanje_izdavatejl(sifra):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM izdavatelj WHERE sifra=%s", sifra)
+    conn.commit()
+    return ("Izdavatelj je obrisan")
+
+@app.route('/nakladnik', methods=["GET"])
+def svi_nakladnici_prikaz():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM nakladnik")
+    nakladnik = cursor.fetchall()
+    conn.commit()
+    return jsonify({'nakladnici' : nakladnik})
+
+@app.route('/nakladnik/<sifra>', methods=["GET"])
+def jedan_nakladnik_prikaz(sifra):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    nakladnik = cursor.execute("SELECT * FROM nakladnik where sifra = %s", sifra)
+    nakladnik = cursor.fetchone()
+    return jsonify({'nakladnici' : nakladnik})
+
+@app.route('/nakladnik', methods=["POST"])
+def dodavanje_nakladnik():
+    naziv = request.json.get('Naziv', None)
+    mjesto = request.json.get('Mjesto', None)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO nakladnik (Naziv, Mjesto) VALUES (%s,%s)", (naziv,mjesto))
+    conn.commit()
+    return ("Novi nakladnik dodan")
+
+@app.route('/nakladnik/<sifra>', methods=["PUT"])
+def edit_nakladnik(sifra):
+    naziv = request.json.get('Naziv', None)
+    mjesto = request.json.get('Mjesto', None)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE nakladnik SET Naziv=%s, Mjesto=%s WHERE sifra =%s", (naziv,mjesto,sifra))
+    cursor.fetchone()
+    conn.commit()
+    return jsonify("Nakladnik je uređen")
+
+@app.route('/nakladnik/<sifra>', methods=["DELETE"])
+def brisanje_nakladnik(sifra):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM nakladnik WHERE sifra=%s", sifra)
+    conn.commit()
+    return ("nakladnik je obrisan")
+
+@app.route('/izdavanje', methods=["GET"])
+def sva_izdavanja_prikaz():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM izdavanje")
+    izdavanje = cursor.fetchall()
+    conn.commit()
+    return jsonify({'nakladnici' : izdavanje})
+
+@app.route('/izdavanje/<sifra>', methods=["GET"])
+def jedno_izdavanje_prikaz(sifra):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    izdavanje = cursor.execute("SELECT * FROM izdavanje where sifra = %s", sifra)
+    izdavanje = cursor.fetchone()
+    return jsonify({'izdavanje' : izdavanje})
+
+@app.route('/izdavanje', methods=["POST"])
+def dodavanje_izdavanje():
+    datum_izdavanja = request.json.get('datum_izdavanja', None)
+    datum_povratka = request.json.get('datum_povratka', None)
+    cijena = request.json.get('cijena', None)
+    izdavatelj = request.json.get('izdavatelj', None)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO izdavanje (datum_izdavanja, datum_povratka, cijena, izdavatelj) VALUES (%s,%s,%s,%s)", (datum_izdavanja,datum_povratka,cijena,izdavatelj))
+    conn.commit()
+    return ("Novo izdavanje dodano")
+
+@app.route('/izdavanje/<sifra>', methods=["PUT"])
+def edit_izdavanje(sifra):
+    datum_izdavanja = request.json.get('datum_izdavanja', None)
+    datum_povratka = request.json.get('datum_povratka', None)
+    cijena = request.json.get('cijena', None)
+    izdavatelj = request.json.get('izdavatelj', None)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE izdavanje SET datum_izdavanja=%s, datum_povratka=%s cijena=%s, izdavatelj=%s WHERE sifra =%s", (datum_izdavanja,datum_povratka,cijena,izdavatelj,sifra))
+    cursor.fetchone()
+    conn.commit()
+    return jsonify("Nakladnik je uređen")
+
+@app.route('/izdavanje/<sifra>', methods=["DELETE"])
+def brisanje_izdavanje(sifra):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM izdavanje WHERE sifra=%s", sifra)
+    conn.commit()
+    return ("Izdavanje je obrisano")
+
 
 
 if __name__ == '__main__':
